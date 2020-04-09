40a92ba559c7e48cf6f08632434d9406461ff371
Sajan <ksajan12@gmail.com>
2020-04-09 00:59:58 -0700

    update

---
+++ RememberMe/app.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+//Card Options
+const cardArray = [
+  {
+    name: 'bacon',
+    img: 'images/bacon.png'
+  },
+  {
+    name: 'burger',
+    img: 'images/burger.png'
+  },
+  {
+    name: 'pizza',
+    img: 'images/pizza.png'
+  },
+  {
+    name: 'roast-chicken',
+    img: 'images/roast-chicken.png'
+  },
+  {
+    name: 'shrimp',
+    img: 'images/shrimp.png'
+  },
+  {
+    name: 'french-fries',
+    img: 'images/french-fries.png'
+  },
+  {
+    name: 'banana',
+    img: 'images/banana.png'
+  },
+  {
+    name: 'pineapple',
+    img: 'images/pineapple.png'
+  },
+  {
+    name: 'fish',
+    img: 'images/fish.png'
+  },
+  {
+    name: 'spaghetti',
+    img: 'images/spaghetti.png'
+  },
+]
+
+)
